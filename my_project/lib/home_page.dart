c3443ddd540ce7a9a9ec1b57d905589d2eb3bc77
Tamzid H Chowdhury <cse_0182320012101173@lus.ac.bd>
2025-11-13 19:20:59 +0600

    Added astronaut image and updated home_page.dart

--- my_project/lib/home_page.dart
+++ my_project/lib/home_page.dart
@@ -27,7 +27,7 @@ class HomePage extends StatelessWidget {
         children: [
           DrawerHeader(
             child: UserAccountsDrawerHeader(
-              decoration: BoxDecoration(color: const Color.fromARGB(255, 141, 33, 25)),
+              decoration: BoxDecoration(color: const Color.fromARGB(255, 37, 128, 40)),
               accountName: Text("Name"),
               accountEmail: Text("Email"),
             ),
@@ -46,24 +46,66 @@ class HomePage extends StatelessWidget {
         ],
       ),
 
-      body: Center(
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Container(
+          height: 500,
+          width: 350,
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [const Color.fromARGB(255, 78, 72, 19), const Color.fromARGB(255, 81, 46, 34)],
+            ),
+            border: Border.all(color: const Color.fromARGB(255, 97, 19, 111), width: 5),
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+          ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 "Welcome to homepage",
                 style: GoogleFonts.lobster(textStyle: TextStyle(fontSize: 30)),
               ),
-              Image.asset('assets/images/flutter.png', height: 50),
-              Image.asset(
-                'assets/images/images.jpeg',
-                height: 400,
-                fit: BoxFit.fill,
+             Image.asset('assets/images/Astronauts.jpg', height: 100),
+
+
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        // backgroundColor: Colors.black,
+                        // foregroundColor: Colors,
+                        // fixedSize: Size(200, 20),
+                      ),
+                      child: Text("Elevated"),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        // backgroundColor: Colors.red,
+                        // foregroundColor: Colors.green,
+                        // fixedSize: Size(200, 20),
+                      ),
+                      child: Text("Text"),
+                    ),
+                    OutlinedButton(onPressed: () {}, child: Text("Outlined")),
+                  ],
+                ),
               ),
-              Image.network(
-                'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500',
-              ),
+              // SizedBox(height: 20),
+              Container(height: 100, width: 100, color: const Color.fromARGB(255, 44, 81, 99)),
+              // Image.asset(
+              //   'assets/images/images.jpeg',
+              //   height: 400,
+              //   fit: BoxFit.fill,
+              // ),
+              // Image.network(
+              //   'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500',
+              // ),
             ],
           ),
         ),
